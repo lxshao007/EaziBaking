b33eabb80a114d04308b4bb6e92129aab996003a
Lingxiao Shao <lingxiaoahao@gmail.com>
2019-05-15 23:53:31 -0700

    added the video player

--- app/src/main/java/com/example/eazibaking/StepDetailFragment.java
+++ app/src/main/java/com/example/eazibaking/StepDetailFragment.java
@@ -1,6 +1,7 @@
 package com.example.eazibaking;
 
 import android.databinding.DataBindingUtil;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -11,6 +12,21 @@ import android.view.ViewGroup;
 
 import com.example.eazibaking.Models.Step;
 import com.example.eazibaking.databinding.FragmentStepDetailBinding;
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.AdaptiveVideoTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
 import com.google.gson.reflect.TypeToken;
 
 public class StepDetailFragment extends Fragment {
@@ -18,10 +34,11 @@ public class StepDetailFragment extends Fragment {
     private static final String ARG_STEP = "ARG_STEP";
 
     private String arg_step;
-
     private StepDetailViewModel mViewModel;
     private FragmentStepDetailBinding binding;
     private Step step;
+    private SimpleExoPlayer mExoPlayer;
+    private SimpleExoPlayerView mSimpleExoPlayerView;
 
     public static StepDetailFragment newInstance(String param) {
         StepDetailFragment fragment = new StepDetailFragment();
@@ -38,6 +55,7 @@ public class StepDetailFragment extends Fragment {
             arg_step = getArguments().getString(ARG_STEP);
             step = ModelUtils.toObject(arg_step, new TypeToken<Step>(){});
         }
+
     }
 
     @Override
@@ -46,10 +64,42 @@ public class StepDetailFragment extends Fragment {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_step_detail, container, false);
         binding.tvShortDescription.setText(step.getShortDescription());
         binding.tvDescription.setText(step.getDescription());
+        initializePlayer(step.getVideoURL());
         return binding.getRoot();
     }
 
-//    @Override
+    private void initializePlayer(String videoUrl) {
+        Uri videoUri = Uri.parse(videoUrl);
+        mSimpleExoPlayerView = binding.exoPlayerView;
+        if (mExoPlayer == null) {
+            BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+            TrackSelector trackSelector = new DefaultTrackSelector(new AdaptiveVideoTrackSelection.Factory(bandwidthMeter));
+            LoadControl loadControl = new DefaultLoadControl();
+            mExoPlayer = ExoPlayerFactory.newSimpleInstance(getContext(), trackSelector, loadControl);
+            mSimpleExoPlayerView.setPlayer(mExoPlayer);
+            mSimpleExoPlayerView.setKeepScreenOn(true);
+            DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(getContext(), "ExoPlayer");
+            MediaSource videoSource = new ExtractorMediaSource(videoUri, dataSourceFactory, new DefaultExtractorsFactory(), null, null);
+            mExoPlayer.prepare(videoSource);
+            mExoPlayer.setPlayWhenReady(true);
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mExoPlayer != null) {
+            mExoPlayer.setPlayWhenReady(false);
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mExoPlayer.release();
+    }
+
+    //    @Override
 //    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
 //        super.onActivityCreated(savedInstanceState);
 //        mViewModel = ViewModelProviders.of(this).get(StepDetailViewModel.class);
